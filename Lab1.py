e40d928a7bddb238022cd61bff248fa22d06c29b
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-04-20 02:58:17 +0500

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,1 +1,29 @@
 
+counter = 100
+miles = 1000.0
+name = "John"
+
+if counter == 100:
+    print("Value of expression is 100")
+print("Good Bye!")
+
+while(counter <9):
+    print('The count is:',counter)
+    counter=counter+1
+
+for letter in 'Python':
+    print ('Current Letter',letter)
+fruits = ['banana','apple','mango']
+
+for fruit in fruits:
+    print ('current fruit: ',fruits)
+#Lab1
+
+Numbers = [5,6,4,2,8,5,9]
+
+print(min(Numbers))
+print(sum(Numbers))
+Numbers.sort()
+print(Numbers)
+
+
