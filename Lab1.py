9e2dd963abab968cfcb190e8d7b6dcabbda3814a
mriceflame <farhanashrafali30@gmail.com>
2021-04-20 03:31:03 +0500

    Practiced

--- Lab1.py
+++ Lab1.py
@@ -19,11 +19,38 @@ for fruit in fruits:
     print ('current fruit: ',fruits)
 #Lab1
 
-Numbers = [5,6,4,2,8,5,9]
+#Task1:
+countryName = input("Enter the country Name: ")
+Countries = ["Pakistan","India","Japan","italy","England","Germany"]
+AI="Artificial Intelligence"
+countrieslen = len(Countries)
+x=0
+for x in range(countrieslen):
+    if countryName in Countries[x]:
+    #  Countries[x]== AI
+     #   Countries.replace(x,AI)
+
+print(Countries)
+#task 2:
+Numbers = [5,6,4,2,8,5,9,10,45,26,98,5,3,4,8,25,45,63,5,31,75,14]
 
 print(min(Numbers))
 print(sum(Numbers))
 Numbers.sort()
 print(Numbers)
 
+#task 3
+ 
+def NumbersLessThanTen():
+    for x in Numbers:
+        if x < 10:
+            print(x)
 
+def NumberGreaterThanfourteen():
+    for x in Numbers:
+        if x > 14:
+            print(x)
+
+NumbersLessThanTen()
+
+NumberGreaterThanfourteen()
